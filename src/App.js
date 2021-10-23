fafca55d144257f2f752607f9d7351590bda2e19
rljayasri <91042044+rljayasri@users.noreply.github.com>
2021-10-22 17:26:13 -0700

    Src files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Homepage from "./components/Homepage";
+import Leftpane from "./components/Leftpane";
+
+function App() {
+  return (
+    <div className="App">
+      <Leftpane />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
