a76a2d9748d9c42c2904dd40a7fc5274c85944a5
rljayasri <91042044+rljayasri@users.noreply.github.com>
2021-10-24 18:28:07 -0700

    Adding image capability

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,33 +5,31 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
-const Post = forwardRef(
-  ({ displayName, title, text, image }, ref) => {
-    return (
-      <div className="post" ref={ref}>
-        <div className="post__body">
-          <div className="post__header">
-            <div className="post__headerText">
-              <h3>
+const Post = forwardRef(({ displayName, title, text, image }, ref) => {
+  return (
+    <div className="post" ref={ref}>
+      <div className="post__body">
+        <div className="post__header">
+          <div className="post__headerText">
+            <h3>
               <span className="post__headerSpecial">@{displayName}</span>
-              </h3>
-            </div>
-            <div className="post__headerDescription">
-              <p>{title}</p>
-              <p>{text}</p>
-            </div>
+            </h3>
           </div>
-          <img src={image} alt="" />
-          <div className="post__footer">
-            <ChatBubbleOutlineIcon fontSize="small" />
-            <RepeatIcon fontSize="small" />
-            <FavoriteBorderIcon fontSize="small" />
-            <PublishIcon fontSize="small" />
+          <div className="post__headerDescription">
+            <p>{title}</p>
+            <p>{text}</p>
           </div>
         </div>
+        <img src={image} alt="" />
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
+        </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
 
 export default Post;
